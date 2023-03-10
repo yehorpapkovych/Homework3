4f7454b5350e0c90fc76e9ffcadae608ccf67fa3
yehorpapkovych <127139325+yehorpapkovych@users.noreply.github.com>
2023-03-10 10:51:44 +0200

    Homework 1

---
+++ Task2.py
@@ -0,0 +1,7 @@
+# K: Hello there!
+print('K: Hello there', end='!\n')
+# G: General Kenobi! You are a bold one!
+print('G: General Kenobi!', 'You are a bold one!')
+# G: Kill him! G: Back away! I will deal with this jedi slime myself!
+# K: Your move
+print('G: Kill him!', 'G: Back away! I will deal with this jedi slime myself!', 'K: Your move', sep='\n')
